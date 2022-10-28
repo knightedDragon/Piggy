f55a0ce38ac752b8f0f57ce21d4b60033d64d73d
Aurora <kathryn.aurora.w@gmail.com>
2022-10-27 17:27:53 -0700

    Made empty class for a smart computer player

--- app/src/main/java/edu/up/cs301/pig/PigMainActivity.java
+++ app/src/main/java/edu/up/cs301/pig/PigMainActivity.java
@@ -40,12 +40,17 @@ public class PigMainActivity extends GameMainActivity {
             }});
         playerTypes.add(new GamePlayerType("Computer Player") {
             public GamePlayer createPlayer(String name) {
+                return new PigSmartCompPlayer(name);
+            }});
+        playerTypes.add(new GamePlayerType("Smart Computer Player") {
+            public GamePlayer createPlayer(String name) {
                 return new PigComputerPlayer(name);
             }});
 
         // Create a game configuration class for Pig:
         GameConfig defaultConfig = new GameConfig(playerTypes, 1, 2, "Pig", PORT_NUMBER);
         defaultConfig.addPlayer("Human", 0); // player 1: a human player
+        defaultConfig.addPlayer("Smart Computer", 1); // player 2: a smart computer player
         defaultConfig.addPlayer("Computer", 1); // player 2: a computer player
         defaultConfig.setRemoteData("Remote Human Player", "", 0);
 
---
+++ app/src/main/java/edu/up/cs301/pig/PigSmartCompPlayer.java
@@ -0,0 +1,20 @@
+package edu.up.cs301.pig;
+
+import edu.up.cs301.game.GameComputerPlayer;
+import edu.up.cs301.game.infoMsg.GameInfo;
+
+public class PigSmartCompPlayer extends GameComputerPlayer {
+    /**
+     * constructor
+     *
+     * @param name the player's name (e.g., "John")
+     */
+    public PigSmartCompPlayer(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void receiveInfo(GameInfo info) {
+
+    }
+}
