416c4ba2cf272cac4165ecdbe475cabf4e3a2dc4
Aurora <kathryn.aurora.w@gmail.com>
2022-10-27 17:32:34 -0700

    Ch 5 (Still can't typecast)

--- app/src/main/java/edu/up/cs301/pig/PigSmartCompPlayer.java
+++ app/src/main/java/edu/up/cs301/pig/PigSmartCompPlayer.java
@@ -1,5 +1,7 @@
 package edu.up.cs301.pig;
 
+import java.util.Random;
+
 import edu.up.cs301.game.GameComputerPlayer;
 import edu.up.cs301.game.infoMsg.GameInfo;
 
@@ -15,6 +17,42 @@ public class PigSmartCompPlayer extends GameComputerPlayer {
 
     @Override
     protected void receiveInfo(GameInfo info) {
+        PigGameState piggy = new PigGameState((PigGameState) info);
+        //Typecast info to make instance of current gamestate
+        if (!(piggy.getId() == this.playerNum)) {
+            return;
+        }
+        this.sleep(1000);
 
-    }
+        int cP = this.playerNum;
+        int hScore, cScore;
+        int total = piggy.getRunTotal();
+
+        if (cP == 0) {
+            cScore = piggy.getP1Score();
+            hScore = piggy.getP2Score();
+        } else {
+            cScore = piggy.getP2Score();
+            hScore = piggy.getP1Score();
+        }
+
+        PigRollAction roll = new PigRollAction(this);
+        PigHoldAction hold = new PigHoldAction(this);
+
+        if (total + cScore >= 50) { //if they'll win, take it
+            game.sendAction(hold);
+        } else if (piggy.getCurrVal() >= 4) {//if die is at 4,5, or 6 hold
+            game.sendAction(hold);
+        } else if (cScore + total >= hScore) { //if puts ahead of/equal to opp take it
+            game.sendAction(hold);
+        } else {
+            Random rand = new Random();
+            int hehe = rand.nextInt(2);
+            if (hehe == 0) {
+                game.sendAction(hold);
+            } else {
+                game.sendAction(roll);
+            }
+        } //else its random
+    }//receiveInfo
 }
